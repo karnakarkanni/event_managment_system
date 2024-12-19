810e71bdf94c9670674e79a4a575f24c8d150d70
karnakarmarati <karnakarmaratikanni@gmail.com>
2024-12-19 12:42:05 +0530

    new

--- Frontend/src/login/customer.jsx
+++ Frontend/src/login/customer.jsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "../../src/login.css"
+
 
 function Customer({ setIsLoggedIn }) {
     const [users, setUsers] = useState([]);
@@ -8,63 +10,78 @@ function Customer({ setIsLoggedIn }) {
 
     useEffect(() => {
         axios.get("https://event-managment-system-4tvs.onrender.com/register")
-            .then((res) => {
-                setUsers(res.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching users data:", error);
-            });
+            .then((res) => setUsers(res.data))
+            .catch((error) => console.error("Error fetching users data:", error));
     }, []);
 
     const userLogin = (e) => {
         e.preventDefault();
         const email = e.target[0].value;
         const password = e.target[1].value;
+        console.log(email,password)
 
         const foundUser = users.find(user => user.email === email && user.password === password);
         const foundUser1 = users.find(user => user.email === email);
 
         if (foundUser) {
-            localStorage.setItem('isLoggedIn', 'true');
+            localStorage.setItem("isLoggedIn", "true");
             setIsLoggedIn(true);
             navigate("/home");
-        } 
-        else if (email === "karnakarmaratikanni@gmail.com" && password === "123") {
+        } else if (email === "karnakarmaratikanni@gmail.com" && password === "123") {
+            localStorage.setItem("isLoggedIn", "true");
+            setIsLoggedIn(true);
             navigate("/admin");
-        } 
-        else if(foundUser1){
-            alert("password is incorrect");
-        }
-        else if(!foundUser) {
-            alert("please register");
+            console.log("hello")
+        } else if (foundUser1) {
+            alert("Password is incorrect");
+        } else {
+            alert("Please register");
         }
     };
+
     const guestLogin = () => {
-        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem("isLoggedIn", "true");
         setIsLoggedIn(true);
         navigate("/home");
     };
 
     return (
-        <>
-            <div id="Customer">
-                <div id="l">
-                    <h1>Login</h1>
-                    <form onSubmit={userLogin}>
-                        <input type="email" placeholder="Email" id="k1" className="input" required /><br /><br />
-                        <input type="password" placeholder="Password" id="k2" className="input" required /><br /><br />
-                        <input type="submit" id="k3" value="Login" className="input"/><br /><br />
-                        <span>Not a member? <Link to="/Registration">Register here</Link></span><br /><br />
-                    </form>
-                    <span onClick={guestLogin} style={{ cursor: 'pointer', color: 'blue' }}>
-                        Login as a Guest
-                    </span><br /><br />
+        <div className="wrapper">
+            <div className="container">
+                <div className="col-left">
+                    <div className="login-text">
+                        <h2>Logo</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget eros dapibus, ultricies tellus vitae, consectetur tortor.
+                        </p>
+                    </div>
+                </div>
+                <div className="col-right">
+                    <div className="login-form">
+                        <h2>Login</h2>
+                        <form onSubmit={userLogin}>
+                            <p>
+                                <input type="email" placeholder="Email"  />
+                            </p>
+                            <p>
+                                <input type="password" placeholder="Password" />
+                            </p>
+                            <p>
+                                <input className="btn" type="submit" value="Sign In" />
+                            </p>
+                            <p>
+                                <Link to="/Registration">Create an account.</Link>
+                            </p>
+                        </form>
+                        <span onClick={guestLogin} style={{ cursor: "pointer", color: "blue" }}>
+                            Login as a Guest
+                        </span>
+                    </div>
                 </div>
             </div>
-        </>
+           
+        </div>
     );
 }
 
 export default Customer;
-
-
